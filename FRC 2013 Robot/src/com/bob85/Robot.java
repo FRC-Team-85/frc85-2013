dbb476c6ff9cbc4b1d5ad34f969daeed58b4f748
Steven <Shadowknight156184@gmail.com>
2013-01-12 12:08:44 -0500

    RR NetTables test code
    Roborealm test code that relays information from NetTables to CRIO.

--- FRC 2013 Robot/src/com/bob85/Robot.java
+++ FRC 2013 Robot/src/com/bob85/Robot.java
@@ -9,6 +9,9 @@ package com.bob85;
 
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.networktables.*;
+import edu.wpi.first.wpilibj.smartdashboard.*;
+import edu.wpi.first.wpilibj.tables.*;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -22,6 +25,10 @@ public class Robot extends IterativeRobot {
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
+    
+    SmartDashboard sDashboard = new SmartDashboard();
+    
+    NetworkTable server = NetworkTable.getTable("SmartDashboard");
     public void robotInit() {
 
     }
@@ -38,6 +45,16 @@ public class Robot extends IterativeRobot {
      */
     public void teleopPeriodic() {
         
+        server.putNumber("test", 15.0);
+            try {
+                
+                    System.out.println(server.getValue("TEST"));// Doesn't work
+                    
+                    System.out.println(server.getNumber("IMAGE_COUNT", 0.0));
+            }
+            
+            catch (TableKeyNotDefinedException ex) {
+            }
     }
     
     /**
