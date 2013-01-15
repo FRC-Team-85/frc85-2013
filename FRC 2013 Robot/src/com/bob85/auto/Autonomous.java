6c97794cbd41e0addd24a7666d5176ae4d6e7b01
Michael Chau <mchau95@gmail.com>
2013-01-15 08:35:05 -0500

    Autonomous class created with skeleton code

---
+++ FRC 2013 Robot/src/com/bob85/auto/Autonomous.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bob85.auto;
+
+/**
+ *
+ * @author Michael Chau <mchau95@gmail.com>
+ */
+public class Autonomous {
+    
+    /**
+     * Skeleton for a test function
+     */
+    private static void testAutoMode() {
+        
+    }
+    
+    /**
+     * Only public access to the class
+     */
+    public static void runAutonomous() {
+        testAutoMode();
+    }
+}
