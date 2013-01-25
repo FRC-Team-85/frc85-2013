2b489f742001a58e45cd5197ce7e962c35e21526
Michael Chau <mchau95@gmail.com>
2013-01-25 17:43:28 -0500

    Added skeleton code DriveCommand
    driveCommand does not have a closed loop drive method

---
+++ FRC 2013 Robot/src/com/bob85/auto/DriveCommand.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bob85.auto;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+
+/**
+ *
+ * @author Michael Chau <mchau95@gmail.com>
+ */
+public class DriveCommand {
+    
+    private RobotDrive m_drive;
+    
+    private double initialDist;
+    private boolean isInitialDistSet;
+    
+    public DriveCommand(RobotDrive drive) {
+        m_drive = drive;
+    }
+    
+    /**
+     * Drives the robot for a desired distance.
+     * Currently waiting on a proper closed loop autonomous drive method
+     * 
+     * @param currentDist current displacement robot is at
+     * @param desiredDist goal displacement of the robot
+     * @param isEnabled is the method enabled
+     */
+    public void driveCommand(boolean isEnabled, double currentDist, double desiredDist) {
+        if (isEnabled) {
+            if (!isInitialDistSet) {
+                initialDist = currentDist;
+                isInitialDistSet = true;
+            }
+            if ((currentDist - initialDist) < desiredDist) {
+                m_drive.drive(1, 0);
+            }
+
+            else {
+                m_drive.drive(0, 0);
+                isInitialDistSet = false;
+            }
+        }
+    }
+}
