9b77a8d76f369ea55c942bc21b56537f1f3dbf3e
Richard Edwards <mivsmn@gmail.com>
2013-01-26 11:06:28 -0500

    Servo Shifting
    for Elevator and add an inElevMode check

--- FRC 2013 Robot/src/com/bob85/pyramidClimb.java
+++ FRC 2013 Robot/src/com/bob85/pyramidClimb.java
@@ -2,17 +2,15 @@ package com.bob85;
 
 import edu.wpi.first.wpilibj.*;
 
-
 public class pyramidClimb {
-    
-    
-    public void elevatorHooks(Encoder elevEnc, Joystick auxStick, int upButton, int downButton, SpeedController leftDriveMotor, SpeedController rightDriveMotor, double elevDriveSpeed) {
-     
-        if (auxStick.getRawButton(upButton) == true) {
+
+    public void elevatorHooks(Encoder elevEnc, Joystick auxStick, int upButton, int downButton, SpeedController leftDriveMotor, SpeedController rightDriveMotor, double elevDriveSpeed, boolean inElevMode) {
+
+        if (inElevMode == true && auxStick.getRawButton(upButton) == true) {
             //Drive Elev Up
             leftDriveMotor.set(elevDriveSpeed);
             rightDriveMotor.set(-elevDriveSpeed);
-        } else if (auxStick.getRawButton(downButton) == true) {
+        } else if (inElevMode == true && auxStick.getRawButton(downButton) == true) {
             //Drive Elev Down
             leftDriveMotor.set(-elevDriveSpeed);
             rightDriveMotor.set(elevDriveSpeed);
@@ -22,10 +20,21 @@ public class pyramidClimb {
             rightDriveMotor.set(0);
         }
 
-}
-    
-    public void driveElevShift(Joystick leftStick, int driveShiftButton, int elevShiftButton, Servo leftShiftServo, Servo rightShiftServo){
-        
     }
-    
+
+    public void driveElevShift(Joystick leftStick, int driveShiftButton, int elevShiftButton, Servo leftShiftServo, Servo rightShiftServo, boolean inElevMode) {
+
+        if (leftStick.getRawButton(driveShiftButton) == true) {
+
+            leftShiftServo.set(0.0);
+            rightShiftServo.set(1.0);
+            inElevMode = false;
+
+        } else if (leftStick.getRawButton(elevShiftButton) == true) {
+
+            leftShiftServo.set(1.0);
+            rightShiftServo.set(0.0);
+            inElevMode = true;
+        }
+    }
 }
