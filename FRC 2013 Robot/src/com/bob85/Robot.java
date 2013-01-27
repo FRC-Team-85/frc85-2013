0845d48feafcb1e87a76ccf7b4f87fb2b7c0681a
Michael Chau <mchau95@gmail.com>
2013-01-27 16:10:31 -0500

    Resolve conflict between master and ElevatorDrive

--- FRC 2013 Robot/src/com/bob85/Robot.java
+++ FRC 2013 Robot/src/com/bob85/Robot.java
@@ -1,12 +1,4 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package com.bob85;
-
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.networktables.*;
@@ -15,34 +7,44 @@ import edu.wpi.first.wpilibj.tables.*;
 
 
 public class Robot extends IterativeRobot {
-  
-    
+
     SmartDashboard sDashboard = new SmartDashboard();
     
     NetworkTable server = NetworkTable.getTable("SmartDashboard");
-    
+
     public void robotInit() {
 
     }
+    
+    public void disabledInit() {
+        
+    }
+    
+    public void autonomousInit() {
+        
+    }
+    
+    public void teleopInit() {
+        
+    }
+    
+    public void testInit() {
+        
+    }
+    
+    public void disabledPeriodic() {
+        
+    }
 
-    /**
-     * This function is called periodically during autonomous
-     */
     public void autonomousPeriodic() {
 
     }
 
-    /**
-     * This function is called periodically during operator control
-     */
     public void teleopPeriodic() {
         
       
     }
     
-    /**
-     * This function is called periodically during test mode
-     */
     public void testPeriodic() {
     
     }
