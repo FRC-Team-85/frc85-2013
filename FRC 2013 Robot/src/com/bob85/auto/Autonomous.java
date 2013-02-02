9dc44a7263c7b8b4b306cde7c205db1dbd3f1006
Richard Edwards <mivsmn@gmail.com>
2013-02-02 14:21:59 -0500

    Added driving directions
    added a constant for waitTime
    added some disk loading mechanism code but commented
    it out

--- FRC 2013 Robot/src/com/bob85/auto/Autonomous.java
+++ FRC 2013 Robot/src/com/bob85/auto/Autonomous.java
@@ -4,36 +4,44 @@
  */
 package com.bob85.auto;
 
+import com.sun.squawk.util.MathUtils;
 import edu.wpi.first.wpilibj.*;
+//import src.com.bob85.FrisbeeLoader;
 
-/**
- *
+/*
  * @author Michael Chau <mchau95@gmail.com>
  */
 public class Autonomous {
 
     Timer autoTimer;
-    double waitTime;
+    double waitTime = 6.5;//in seconds; this is an approximate
+    double uWaitTime = (waitTime * MathUtils.pow(10.0, 6.0)); //in microSeconds (uSeconds)
 
     public void stageAutoDrive(double driveSpeed, SpeedController leftFrontSpControl, SpeedController leftBackSpControl, SpeedController rightFrontSpControl, SpeedController rightBackSpControl, Encoder leftDriveEnc, Encoder rightDriveEnc) {
-        if (((leftDriveEnc.get() + rightDriveEnc.get()) / 2) < 200) { //twelveInches~200encCounts
-            leftFrontSpControl.set(driveSpeed);
-            leftBackSpControl.set(driveSpeed);
-            rightFrontSpControl.set(driveSpeed);
-            leftBackSpControl.set(driveSpeed);
-        } else {
-            leftFrontSpControl.set(0.0);
-            leftBackSpControl.set(0.0);
-            rightFrontSpControl.set(0.0);
-            rightBackSpControl.set(0.0);
-            autoTimer.start();
-        }
+        /*if (((leftDriveEnc.get() + rightDriveEnc.get()) / 2) < 200) { //twelveInches~200encCounts
+         leftFrontSpControl.set(driveSpeed);
+         leftBackSpControl.set(driveSpeed);
+         rightFrontSpControl.set(driveSpeed);
+         leftBackSpControl.set(driveSpeed);
+         } else {
+         leftFrontSpControl.set(0.0);
+         leftBackSpControl.set(0.0);
+         rightFrontSpControl.set(0.0);
+         rightBackSpControl.set(0.0);
+         autoTimer.start();
+         }*/
+        autoTimer.start();
 
-        if (autoTimer.get() > waitTime && ((leftDriveEnc.get() + rightDriveEnc.get()) / 2) > 0) {
+        if (autoTimer.get() > uWaitTime && ((leftDriveEnc.get() + rightDriveEnc.get()) / 2) > -100) {
             leftFrontSpControl.set(-driveSpeed);
             leftBackSpControl.set(-driveSpeed);
             rightFrontSpControl.set(-driveSpeed);
             rightBackSpControl.set(-driveSpeed);
+        } else if (autoTimer.get() > uWaitTime && ((leftDriveEnc.get() + rightDriveEnc.get()) / 2) == -100) {
+            leftFrontSpControl.set(1.0);
+            leftBackSpControl.set(1.0);
+            rightFrontSpControl.set(-1.0);
+            rightBackSpControl.set(-1.0);
         } else {
             leftFrontSpControl.set(0.0);
             leftBackSpControl.set(0.0);
@@ -42,14 +50,19 @@ public class Autonomous {
         }
     }
 
-    public void stageAutoShoot(double shooterSpeed, SpeedController shooterMotor) {
-        if (autoTimer.get() > 0) {
+    public void stageAutoShoot(double shooterSpeed, SpeedController shooterMotor, Servo dropServo, Servo readyServo) {
+        if (autoTimer.get() > 0.0) { //Timer.get() is in microSecinds
             shooterMotor.set(shooterSpeed);
+            //unlockServo(dropServo); 
+            //unlockServo(readyServo);
             //unfinished, write for disk loader mechanism
-            
-
+        } else if (autoTimer.get() >= uWaitTime) {
+            shooterMotor.set(0.0);
+            //lockServo(readyServo);
         } else {
             shooterMotor.set(0.0);
+            //lockServo(dropServo);
+            //lockServo(readyServo);
         }
     }
 
