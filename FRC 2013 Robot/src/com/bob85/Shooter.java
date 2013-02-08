1e569f9151ac9613497658be0c732515a7986cd5
Michael Chau <mchau95@gmail.com>
2013-02-08 06:26:24 -0500

    Added skeleton of auto shooter speed calculation

--- FRC 2013 Robot/src/com/bob85/Shooter.java
+++ FRC 2013 Robot/src/com/bob85/Shooter.java
@@ -18,21 +18,23 @@ public class Shooter {
     public static int SHOOTER_RPM_SENSOR_SLOT = 1;
     public static int SHOOTER_RPM_SENSOR_CHANNEL = 5;
     
-    private double _shooterPID_kP = 0.009;
-    private double _shooterPID_kI = 0.0001;
-    private double _shooterPID_kD = 0;
-    private double _shooterPID_kF = 0;
-    private double _shooterPID_kMinInput = 0;
-    private double _shooterPID_kMaxInput = 3000;
-    private double _shooterPID_kMinOutput = 0;
-    private double _shooterPID_kMaxOutput = 1;
-    private double _shooterPID_kSetPoint = 2800;
+    private double shooterPID_kP = 0.009;
+    private double shooterPID_kI = 0.0001;
+    private double shooterPID_kD = 0;
+    private double shooterPID_kF = 0;
+    private double shooterPID_kMinInput = 0;
+    private double shooterPID_kMaxInput = 3000;
+    private double shooterPID_kMinOutput = 0;
+    private double shooterPID_kMaxOutput = 1;
+    private double shooterPID_kSetPoint = 2800;
     
     private PIDController shooterPID;
     
     private Victor shooterMotor;
     
     private HallEffect shooterSensor;
+    
+    private Joystick joystick;
     
     public Shooter(Victor shooterMotor, PIDController shooterPID, HallEffect shooterSensor) {
         this.shooterMotor = shooterMotor;
@@ -41,13 +43,30 @@ public class Shooter {
         initPIDConstants();
     }
     
+    public Shooter(Victor shooterMotor, PIDController shooterPID, HallEffect shooterSensor, 
+            Joystick joystick) {
+        this.shooterMotor = shooterMotor;
+        this.shooterPID = shooterPID;
+        this.shooterSensor = shooterSensor;
+        this.joystick = joystick;
+        initPIDConstants();
+    }
+    
     /**
      * Initializes Shooter PID Controller Settings
      */
     private void initPIDConstants() {
-        shooterPID.setPID(_shooterPID_kP, _shooterPID_kI, _shooterPID_kD, _shooterPID_kF);
-        shooterPID.setInputRange(_shooterPID_kMinInput, _shooterPID_kMaxInput);
-        shooterPID.setOutputRange(_shooterPID_kMinOutput, _shooterPID_kMaxOutput);
+        shooterPID.setPID(shooterPID_kP, shooterPID_kI, shooterPID_kD, shooterPID_kF);
+        shooterPID.setInputRange(shooterPID_kMinInput, shooterPID_kMaxInput);
+        shooterPID.setOutputRange(shooterPID_kMinOutput, shooterPID_kMaxOutput);
+    }
+    
+    /**
+     * Calculates a shooter RPM SetPoint based on distance away from target
+     * @param distance distance from target in inches
+     */
+    private void calculateShooterSpeed(double distance) {
+        shooterPID_kSetPoint = distance;
     }
     
     /**
@@ -56,7 +75,7 @@ public class Shooter {
     private void initPID() {
         if (!shooterPID.isEnable()) {
             shooterPID.enable();
-            shooterPID.setSetpoint(_shooterPID_kSetPoint);
+            shooterPID.setSetpoint(shooterPID_kSetPoint);
         }
     }
     
@@ -99,4 +118,25 @@ public class Shooter {
         }
     }
     
+    /**
+     * Calculates shooter speed in RPM and use it in a PID Controller
+     * @param isEnabled
+     * @param distance 
+     */
+    private void testAutomaticShooterSpeedCalculation(boolean isEnabled, double distance) {
+        if (isEnabled) {
+            calculateShooterSpeed(distance);
+            initPID();
+        } else {
+            disablePID();
+        }
+    }
+    
+    /**
+     * Runs a shooter feature needed to be tested
+     * @param distance distance from target in inches
+     */
+    public void runCurrentTest(double distance) {
+        testAutomaticShooterSpeedCalculation(joystick.getTrigger(), distance);
+    }
 }
