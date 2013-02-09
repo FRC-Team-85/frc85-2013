c6153a4d8bc40c3c453302797d8a6c1fd36bede7
Richard Edwards <mivsmn@gmail.com>
2013-02-09 11:08:21 -0500

    Tweaked the autonomous
    but need to test and get conversion rates, also initiate stages

--- FRC 2013 Robot/src/com/bob85/auto/Autonomous.java
+++ FRC 2013 Robot/src/com/bob85/auto/Autonomous.java
@@ -16,13 +16,12 @@ public class Autonomous {
     Timer autoTimer;
     double waitTime = 6.5;//in seconds; this is an approximate
     double uWaitTime = (waitTime * MathUtils.pow(10.0, 6.0)); //in microSeconds (uSeconds)
-
     AutoModeChooser autoChooser;
-    
+
     public Autonomous(AutoModeChooser autoChooser) {
         this.autoChooser = autoChooser;
     }
-    
+
     public void stageAutoDrive(double driveSpeed, SpeedController leftFrontSpControl, SpeedController leftBackSpControl, SpeedController rightFrontSpControl, SpeedController rightBackSpControl, Encoder leftDriveEnc, Encoder rightDriveEnc) {
         /*if (((leftDriveEnc.get() + rightDriveEnc.get()) / 2) < 200) { //twelveInches~200encCounts
          leftFrontSpControl.set(driveSpeed);
@@ -36,18 +35,24 @@ public class Autonomous {
          rightBackSpControl.set(0.0);
          autoTimer.start();
          }*/
+       //Need to switch to stages
         autoTimer.start();
-
-        if (autoTimer.get() > uWaitTime && ((leftDriveEnc.get() + rightDriveEnc.get()) / 2) > -100) {
-            leftFrontSpControl.set(-driveSpeed);
+//Timer.get() is in microSeconds
+        if (autoTimer.get() > uWaitTime && ((leftDriveEnc.get() + rightDriveEnc.get()) / 2) > -100) { //stage2-->4?
+            leftFrontSpControl.set(-driveSpeed); 
             leftBackSpControl.set(-driveSpeed);
             rightFrontSpControl.set(-driveSpeed);
             rightBackSpControl.set(-driveSpeed);
-        } else if (autoTimer.get() > uWaitTime && ((leftDriveEnc.get() + rightDriveEnc.get()) / 2) == -100) {
+        } else if (autoTimer.get() > uWaitTime && ((leftDriveEnc.get() + rightDriveEnc.get()) / 2) == -100) { //need to check this by testing
             leftFrontSpControl.set(1.0);
             leftBackSpControl.set(1.0);
             rightFrontSpControl.set(-1.0);
             rightBackSpControl.set(-1.0);
+        } else if (autoTimer.get() == (uWaitTime * 1.5)) {
+            leftFrontSpControl.set(0.0);
+            leftBackSpControl.set(0.0);
+            rightFrontSpControl.set(0.0);
+            rightBackSpControl.set(0.0);
         } else {
             leftFrontSpControl.set(0.0);
             leftBackSpControl.set(0.0);
@@ -57,11 +62,11 @@ public class Autonomous {
     }
 
     public void stageAutoShoot(double shooterSpeed, SpeedController shooterMotor, Servo dropServo, Servo readyServo) {
-        if (autoTimer.get() > 0.0) { //Timer.get() is in microSecinds
+        if (autoTimer.get() > 0.0) { //stage1
             shooterMotor.set(shooterSpeed);
             //unlockServo(dropServo); 
             //unlockServo(readyServo);
-            //unfinished, write for disk loader mechanism
+            //~unfinished, write for disk loader mechanism
         } else if (autoTimer.get() >= uWaitTime) {
             shooterMotor.set(0.0);
             //lockServo(readyServo);
@@ -84,7 +89,7 @@ public class Autonomous {
     public void runAutonomous() {
         testAutoMode();
     }
-    
+
     public void initAutonomous() {
         autoChooser.testDriveStationInputs();
     }
