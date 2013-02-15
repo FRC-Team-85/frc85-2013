2e123c63f4f8ea1fb95e2d124b5c9ff9667bb454
Michael Chau <mchau95@gmail.com>
2013-02-14 19:42:43 -0500

    add ShootCommand class

---
+++ FRC 2013 Robot/src/com/bob85/auto/ShootCommand.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bob85.auto;
+
+/**
+ *
+ * @author Michael Chau <mchau95@gmail.com>
+ */
+public class ShootCommand {
+    private int frisbees; //Amount of frisbees to fire
+    
+    public ShootCommand(int frisbees) {
+        this.frisbees = frisbees;
+    }
+}
