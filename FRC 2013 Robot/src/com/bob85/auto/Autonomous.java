5dbdc7609e475b2b634942dbacbc3beb40f7dbcb
Alex <20081392@stu.zps.org>
2013-02-18 09:34:19 -0500

    moar removed comments

--- FRC 2013 Robot/src/com/bob85/auto/Autonomous.java
+++ FRC 2013 Robot/src/com/bob85/auto/Autonomous.java
@@ -1,8 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.bob85.auto;
+\package com.bob85.auto;
 
 import com.bob85.Drive;
 import com.sun.squawk.util.MathUtils;
