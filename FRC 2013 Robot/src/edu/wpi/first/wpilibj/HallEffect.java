16f91d04d3363b399e186f5e68ad2abe63e0aa05
Michael Chau <mchau95@gmail.com>
2013-02-28 16:46:39 -0500

    Add IIR Filter for Halleffect

--- FRC 2013 Robot/src/edu/wpi/first/wpilibj/HallEffect.java
+++ FRC 2013 Robot/src/edu/wpi/first/wpilibj/HallEffect.java
@@ -4,12 +4,21 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindowSendable;
 
 public class HallEffect extends Counter implements CounterBase, PIDSource, LiveWindowSendable{
 
+    private int IIRFilterCount;
+    private final double kIIRFilter = 1;
+    
     public HallEffect(int channel) {
         super(channel);
     }
     
     public int getRPM() {
         return (int) ((60/getPeriod()));
+    }
+    
+    public int getIIRFilterRPM() {
+        IIRFilterCount = (int)(kIIRFilter * IIRFilterCount + (1-kIIRFilter)*get());
+        reset();
+        return (int) (IIRFilterCount / 0.02 * 60);
     }
 
     public double pidGet() {
